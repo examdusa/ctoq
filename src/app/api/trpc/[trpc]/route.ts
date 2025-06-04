57c1b8220b7fa2cf7b3305bc9c43dad814d26bab
Mayur Karmakar <mayurkarmakar07@gmail.com>
2025-06-04 21:42:22 +0530

    Refactor CORS handling: remove inline CORS headers from TRPC route and add configuration in vercel.json

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,31 +2,11 @@ import { appRouter } from "@/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 const handler = (req: Request) => {
-  if (req.method === "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: {
-        "Access-Control-Allow-Origin":
-          "http://localhost:3000, http://localhost:8080, https://www.content2quiz.com, https://www.thelearn.online",
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });
-  }
-
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
     createContext: () => ({}),
-    responseMeta: () => ({
-      headers: {
-        "Access-Control-Allow-Origin":
-          "http://localhost:3000, http://localhost:8080, https://www.content2quiz.com, https://www.thelearn.online",
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    }),
   });
 };
 
